e072a79d0c60f35a8a127a9f334f430817117f02
devrengaw <lucas.wagneer@gmail.com>
2025-10-29 17:22:40 -0300

    Update pluggy.js

--- pluggy.js
+++ pluggy.js
@@ -1,12 +1,12 @@
 // pluggy.js
-import Pluggy from "pluggy-sdk"
-import dotenv from "dotenv"
+import Pluggy from 'pluggy-sdk'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
 const pluggyClient = new Pluggy.PluggyClient({
   clientId: process.env.PLUGGY_CLIENT_ID,
-  clientSecret: process.env.PLUGGY_CLIENT_SECRET,
+  clientSecret: process.env.PLUGGY_CLIENT_SECRET
 })
 
 export default pluggyClient
