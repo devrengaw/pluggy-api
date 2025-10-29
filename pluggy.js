c90b8bf5e99f6f616ee07fa5ff8e5fa11f7b4546
devrengaw <lucas.wagneer@gmail.com>
2025-10-29 11:02:23 -0300

    pluggy.js

---
+++ pluggy.js
@@ -0,0 +1,11 @@
+// pluggy.js
+import Pluggy from "pluggy-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pluggyClient = new Pluggy({
+  clientId: process.env.PLUGGY_CLIENT_ID,
+  clientSecret: process.env.PLUGGY_CLIENT_SECRET,
+});
+
+export default pluggyClient;
